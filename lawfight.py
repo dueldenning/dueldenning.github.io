8f9067e271c440f5a3fc8ec6477836add400d252
novel <dueldenning@gmail.com>
2017-07-24 21:47:19 +0100

    First batch of files.

---
+++ lawfight.py
@@ -0,0 +1,48 @@
+import json, threading
+from bottle_0_12_13 import app, request, response, route, run
+import engine
+
+HOSTNAME = "127.0.0.1"
+PORT = 8000
+
+WS_ADDRESS = "127.0.0.1"
+WS_PORT = 8005
+
+app.catchall = False
+
+# ----------------------------------------------------------
+
+@route("/", "GET")
+
+def frontpage():
+	return open("pages/index.html").read()
+
+# ----------------------------------------------------------
+
+@route("/game/<game>/id/<id>/player/<player>", "GET")
+
+def play(game, id, player):
+
+	try:
+		html = open("games/{}.html".format(game)).read()
+	except:
+		return "Game not known"
+
+	websocket_connection_js = "var conn = new WebSocket('ws://{}:{}/level/{}/id/{}/player/{}');".format(
+			WS_ADDRESS, WS_PORT, game, id, player
+	)
+
+	html = html.replace("WEBSOCKET_CONNECTION_JS", websocket_connection_js)
+
+	return html
+
+# ----------------------------------------------------------
+
+def main():
+	threading.Thread(target = engine.start, daemon = True, args = (HOSTNAME, WS_PORT)).start()
+	run(host = HOSTNAME, port = PORT)
+
+# ----------------------------------------------------------
+
+if __name__ == "__main__":
+	main()
